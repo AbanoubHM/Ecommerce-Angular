0488e61082b01b87bb7059ae30a7e2fc25c23d4e
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-21 06:30:54 +0200

    material icons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,12 +9,14 @@ import { CartService } from 'src/app/Service/cart.service';
 export class NavbarComponent implements OnInit {
  public totalItem : number =0;
   constructor(private cartService: CartService) { }
-
+  hidden = true;
   ngOnInit(): void {
     this.cartService.getProducts().
     subscribe(res=>{
       this.totalItem = res.length;
+      if(this.totalItem>0){this.hidden=false}else{this.hidden=true}
     })
   }
 
+  
 }
